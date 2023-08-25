c5c367cfe5c05019ef89ceae1958b370974895d4
Devin <devhunter16@gmail.com>
2023-08-25 11:04:57 -0700

    Changed code so that notification banner cannot be seen as it was obnoxious.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { sendCartData } from './store/cart-slice';
-
-// Defining a variable here that will help to NOT send the cart data a soon as we
-// initially load the page so that we dont get a "Sent cart data successfully" right
-// off the bat which would be confusing.
-let isInitial = true;
+import { sendCartData, fetchCartData } from './store/cart-actions';
 
 function App() {
   const dispatch = useDispatch()
@@ -29,22 +24,19 @@ function App() {
   // our Redux data store so that whenever the Redux store does change, our "cart"
   // variable in this file changes as well.
   useEffect(() => {
-    // If this is the first time loading the page, stop here so we don't send the empty
-    // cart data and get a confusing notification telling us the cart data was sent.
-    if (isInitial) {
-      // Set isInitial to false so that we don't everr hit this if statement again after
-      // initially loading our page.
-      isInitial = false;
-      return;
+    if (cart.changed) {
+      // Dispatching out sendCartData() function with the current state of the cart to our
+      // Redux data store. the sendCartData() function returns a function. With redux-toolkit,
+      // when we dispatch an action (in this case a function) which returns a function, it
+      // will automatically execute the returned function and give us the "dispatch" argument
+      // automatically (see cart-slice to see what I mean).
+      dispatch(sendCartData(cart));
     };
+  }, [cart, dispatch]);
 
-    // Dispatching out sendCartData() function with the current state of the cart to our
-    // Redux data store. the sendCartData() function returns a function. With redux-toolkit,
-    // when we dispatch an action (in this case a function) which returns a function, it
-    // will automatically execute the returned function and give us the "dispatch" argument
-    // automatically (see cart-slice to see what I mean).
-    dispatch(sendCartData(cart));
-  }, [cart]);
+  useEffect(() => {
+    dispatch(fetchCartData(cart));
+  }, []);
 
   // Our initial state in the ui data store slice for notification is null, but if the'
   // state changes to not null we then show the notification.
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,10 +1,10 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { uiActions } from './ui-slice';
 
 // Defining our starting cart state
 const initialCartState = {
     items: [],
     totalQuantity: 0,
+    changed: false
 };
 
 // createSlice prepares a slice of our global state. It takes an object as an argument.
@@ -29,6 +29,7 @@ const cartSlice = createSlice({
             // creating a new one.
             const existingItem = state.items.find(item => item.id === newItem.id);
             state.totalQuantity++;
+            state.changed = true;
             // If we do not find the item within our array, then we add it to the array.
             if (!existingItem) {
                 state.items.push({
@@ -49,6 +50,7 @@ const cartSlice = createSlice({
             // and if it does we store that items in the existingItem variable.
             const existingItem = state.items.find(item => item.id === id);
             state.totalQuantity--;
+            state.changed = true;
             if (existingItem.quantity === 1) {
                 // Filtering our items array by finding the item in the array that's id
                 // matches the id we passed into removeItemFromCart, then state.items
@@ -61,57 +63,6 @@ const cartSlice = createSlice({
         }
     }
 });
-
-// This is an action creator. It returns another function that recieves the dispatch
-// function as an argument. 
-export const sendCartData = (cart) => {
-    return async (dispatch) => {
-        // Dispatching an action (in this case a notification) to the user.
-        dispatch(uiActions.showNotification({
-            status: 'pending',
-            title: 'Sending...',
-            message: 'Sending cart data!'
-        }));
-
-        const sendRequest = async () => {
-
-            // We didn't have the "cart" node defined in Firebase before, but by adding
-            // "cart.json" to "https://react-http-1b7e7-default-rtdb.firebaseio.com/" that node 
-            // is created for us in Firebase. We use a PUT rather than a POST request here because
-            // that way the new data will not be added in a list of data, but rather it will 
-            // override the existing data with the incoming data, which is what we want to do in
-            // this app.
-            const response = await fetch(
-                'https://react-http-1b7e7-default-rtdb.firebaseio.com/cart.json',
-                { method: 'PUT', body: JSON.stringify(cart) }
-            );
-
-            // If there is anything wrong with the response throw an error, otherwise continue.
-            if (!response.ok) {
-                throw new Error('Sending cart data failed.');
-            };
-        };
-
-        try {
-            await sendRequest();
-
-            // Dispatching a notification to the user if we successfully send the req.
-            dispatch(uiActions.showNotification({
-                status: 'success',
-                title: 'Success!',
-                message: 'Sent cart data successfully!'
-            }));
-        } catch (error) {
-            // Because sendCartData is an async function it returns a promise, so we can call
-            // catch on it. Here we're catching any errors that slipped past out if statement.
-            dispatch(uiActions.showNotification({
-                status: 'error',
-                title: 'Error!',
-                message: 'Sending cart data failed.'
-            }));
-        }
-    };
-};
 
 // Exporting cartSlice.actions here which gives us access to actions that
 // automatically trigger our reducer functions we defined using createSlice().
